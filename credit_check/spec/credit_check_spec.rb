e36b46b2238e043104fa51771521b717647a44a6
Arnaldo Aparicio <apaarn2211@gmail.com>
2024-03-01 10:13:48 -0500

    Add test to check for attributes

--- credit_check/spec/credit_check_spec.rb
+++ credit_check/spec/credit_check_spec.rb
@@ -7,4 +7,11 @@ RSpec.describe CreditCheck do
 
     expect(credit_check).to be_a CreditCheck
   end
+
+  it 'has a number and a limit' do
+    credit_check = CreditCheck.new("5541808923795240",15000)
+
+    expect(credit_check.card_number).to eq("5541808923795240")
+    expect(credit_check.limit).to eq(15000)
+  end
 end
