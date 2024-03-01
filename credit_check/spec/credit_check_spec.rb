3f1092c5f68d5e4213dcc14e3c71ecd0509c3142
Arnaldo Aparicio <apaarn2211@gmail.com>
2024-03-01 10:11:50 -0500

    Add test to check if CreditCheck exists

--- credit_check/spec/credit_check_spec.rb
+++ credit_check/spec/credit_check_spec.rb
@@ -1,1 +1,10 @@
+require 'rspec'
 require './lib/credit_check'
+
+RSpec.describe CreditCheck do
+  it 'exists' do
+    credit_check = CreditCheck.new("5541808923795240",15000)
+
+    expect(credit_check).to be_a CreditCheck
+  end
+end
