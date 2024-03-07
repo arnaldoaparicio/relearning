3b88ea006874809a960bc4777a7917599799e237
Arnaldo Aparicio <apaarn2211@gmail.com>
2024-03-07 14:04:49 -0500

    Add attribute 'registered_vehicles' to initializer and attr_reader

--- the_dmv/lib/facility.rb
+++ the_dmv/lib/facility.rb
@@ -1,11 +1,12 @@
 class Facility
-  attr_reader :name, :address, :phone, :services
+  attr_reader :name, :address, :phone, :services, :registered_vehicles
 
   def initialize(facility_data)
     @name = facility_data[:name]
     @address = facility_data[:address]
     @phone = facility_data[:phone]
     @services = []
+    @registered_vehicles = []
   end
 
   def add_service(service)
