d9aedb0b6d37826f4846e787da14bd5041f2e7c5
Arnaldo Aparicio <apaarn2211@gmail.com>
2024-03-15 08:46:56 -0400

    Add test to check for successful completion of written driver's test

--- the_dmv/spec/registrant_spec.rb
+++ the_dmv/spec/registrant_spec.rb
@@ -32,4 +32,24 @@ RSpec.describe Registrant do
       expect(@registrant_2.permit?).to eq(true)
     end
   end
+
+  describe '#written test' do
+    it 'successfully administers written test' do
+      registrant_1 = Registrant.new('Bruce', 18, true)
+      registrant_2 = Registrant.new('Penny', 16 )
+      registrant_3 = Registrant.new('Tucker', 15 )
+  
+      facility_1 = Facility.new({name: 'DMV Tremont Branch', address: '2855 Tremont Place Suite 118 Denver CO 80205', phone: '(720) 865-4600'})
+      facility_2 = Facility.new({name: 'DMV Northeast Branch', address: '4685 Peoria Street Suite 101 Denver CO 80239', phone: '(720) 865-4600'})
+      expect(registrant_1.license_data).to eq({:written=>false, :license=>false, :renewed=>false})
+      expect(registrant_1.permit?).to eq(true)
+      expect(facility_1.administer_written_test(registrant_1)).to eq(false)
+      expect(registrant_1.license_data).to eq({:written=>false, :license=>false, :renewed=>false})
+
+      facility_1.add_service('Written Test')
+
+      expect(facility_1.administer_written_test(registrant_1)).to eq(true)
+      expect(registrant_1.license_data).to eq({:written=>true, :license=>false, :renewed=>false})
+    end
+  end
 end
